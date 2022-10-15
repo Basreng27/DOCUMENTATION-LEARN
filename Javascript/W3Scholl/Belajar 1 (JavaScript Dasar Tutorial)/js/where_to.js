954519a947e8704ae71b3117412d7f055aa61132
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2022-10-15 10:24:32 +0700

    up win 11

---
+++ Javascript/W3Scholl/Belajar 1 (JavaScript Dasar Tutorial)/js/where_to.js
@@ -0,0 +1,6 @@
+// Skrip eksternal tidak boleh berisi tag <script>.
+// function js
+function luar() {
+    // mengambil dan mengganti isi tag dengan id "luarfile"
+    document.getElementById("luarfile").innerHTML = "Luar File";
+}

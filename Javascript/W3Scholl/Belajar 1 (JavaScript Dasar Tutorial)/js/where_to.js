aef6d751ebe847befba78d11a13463e1161c22a0
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2022-10-19 13:32:26 +0700

    mengeluarkan folder history dan javascript

--- Javascript/W3Scholl/Belajar 1 (JavaScript Dasar Tutorial)/js/where_to.js
+++
@@ -1,6 +0,0 @@
-// Skrip eksternal tidak boleh berisi tag <script>.
-// function js
-function luar() {
-    // mengambil dan mengganti isi tag dengan id "luarfile"
-    document.getElementById("luarfile").innerHTML = "Luar File";
-}
